95ee29fd5e3bba0cdae70d7be7cbfd2d8472db13
Maksymilian Ulanecki <maks.u@vp.pl>
2024-05-30 17:54:38 +0200

    changed currency type to string, added getter, setter and lombok builder

--- src/main/java/com/exchratenbp/domain/currency/CurrencyRequest.java
+++ src/main/java/com/exchratenbp/domain/currency/CurrencyRequest.java
@@ -1,13 +1,19 @@
 package com.exchratenbp.domain.currency;
 
 import com.exchratenbp.domain.currency.dto.CurrencyRequestDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Builder
 class CurrencyRequest {
 
     private Long id;
-    private CurrencyRate currency;
+    private String currency;
     private String name;
     private LocalDateTime date;
     private Double value;
@@ -15,9 +21,9 @@ class CurrencyRequest {
 
     CurrencyRequestDto toDto() {
         return CurrencyRequestDto.builder()
-                .currency(currency.name())
+                .currency(currency)
                 .name(name)
-                .date(date)
+                .date(date.toString())
                 .value(value)
                 .build();
     }
