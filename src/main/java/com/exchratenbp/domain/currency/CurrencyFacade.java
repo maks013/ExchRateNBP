d20a35312ffb9a1d2911deecfca0f367cadef7d5
Maksymilian Ulanecki <maks.u@vp.pl>
2024-05-30 18:07:38 +0200

    implemented facade methods

--- src/main/java/com/exchratenbp/domain/currency/CurrencyFacade.java
+++ src/main/java/com/exchratenbp/domain/currency/CurrencyFacade.java
@@ -1,4 +1,30 @@
 package com.exchratenbp.domain.currency;
 
+import com.exchratenbp.domain.currency.dto.CurrencyRequestDto;
+import com.exchratenbp.domain.currency.dto.CurrencyResponse;
+import com.exchratenbp.domain.currency.dto.CurrencyValueRequestDto;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class CurrencyFacade {
+
+    private final CurrencyRepository currencyRepository;
+    private final CurrencyService currencyService;
+
+    public List<CurrencyRequestDto> findAllCurrencyRequests() {
+        return currencyRepository.findAll()
+                .stream()
+                .map(CurrencyRequest::toDto)
+                .toList();
+    }
+
+    public CurrencyResponse getCurrencyValue(CurrencyValueRequestDto currencyValueRequestDto) {
+
+        final CurrencyRequestDto currencyRequestDto = currencyService
+                .fetchCurrencyRateValueAndSaveRequest(currencyValueRequestDto).toDto();
+
+        return new CurrencyResponse(currencyRequestDto.value());
+    }
 }
