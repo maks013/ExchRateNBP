6745cbdefc8a4baab93e4da842e2ca8c950421c0
Maksymilian Ulanecki <maks.u@vp.pl>
2024-05-31 19:58:33 +0200

    refactor currency code validator

--- src/main/java/com/exchratenbp/domain/currency/CurrencyCodeValidator.java
+++ src/main/java/com/exchratenbp/domain/currency/CurrencyCodeValidator.java
@@ -2,19 +2,28 @@ package com.exchratenbp.domain.currency;
 
 import com.exchratenbp.domain.currency.exception.InvalidCurrencyException;
 
-public enum CurrencyCodeValidator {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-    THB, USD, AUD, HKD, CAD, NZD, SGD,
-    EUR, HUF, CHF, GBP, UAH, JPY, CZK,
-    DKK, ISK, NOK, SEK, RON, BGN, TRY,
-    ILS, CLP, PHP, MXN, ZAR, BRL, MYR,
-    IDR, INR, KRW, CNY, XDR;
+class CurrencyCodeValidator {
 
-    public static void isCurrencyCodeValid(String currency) {
-        try {
-            CurrencyCodeValidator.valueOf(currency.toUpperCase());
-        } catch (IllegalArgumentException exception) {
-            throw new InvalidCurrencyException(currency);
+    private static final Set<String> VALID_CURRENCIES =
+            new HashSet<>(Arrays.asList(
+                    "THB", "USD", "AUD", "HKD", "CAD", "NZD", "SGD",
+                    "EUR", "HUF", "CHF", "GBP", "UAH", "JPY", "CZK",
+                    "DKK", "ISK", "NOK", "SEK", "RON", "BGN", "TRY",
+                    "ILS", "CLP", "PHP", "MXN", "ZAR", "BRL", "MYR",
+                    "IDR", "INR", "KRW", "CNY", "XDR"
+            ));
+
+    static void isCurrencyCodeValid(String currency) {
+        if (currency != null && !currency.isEmpty()) {
+            if (!VALID_CURRENCIES.contains(currency.toUpperCase())) {
+                throw new InvalidCurrencyException(currency);
+            }
+        } else {
+            throw new InvalidCurrencyException("empty or null");
         }
     }
 }
